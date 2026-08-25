b9db89b0a1ad3c1307aeece59b4aade1c7bcf2f8
veilsun. <87560864+beyondthesunveil@users.noreply.github.com>
2026-08-25 09:03:57 +0200

    Update memberlist.js

--- TEMPLATES/MEMBERLIST BODY/memberlist.js
+++ TEMPLATES/MEMBERLIST BODY/memberlist.js
@@ -1,89 +1,37 @@
-(function () {
-  "use strict";
+document.addEventListener("DOMContentLoaded", function () {
 
-  function initializeLitsoMemberlist() {
-    var memberCards = document.querySelectorAll(
-      "[data-litso-memberlist_member]"
+  document.querySelectorAll(
+    ".litso-memberlist_pseudo"
+  ).forEach(function (pseudo) {
+
+    var groupSpan = pseudo.querySelector(
+      ".usr_grp_clr"
     );
 
-    if (!memberCards.length) {
+    if (!groupSpan) {
       return;
     }
 
-    Array.prototype.forEach.call(
-      memberCards,
-      function (memberCard) {
-        if (
-          memberCard.getAttribute(
-            "data-litso-memberlist-ready"
-          ) === "true"
-        ) {
-          return;
-        }
+    var groupClass = Array.from(
+      groupSpan.classList
+    ).find(function (className) {
+      return className.startsWith("group-");
+    });
 
-        memberCard.setAttribute(
-          "data-litso-memberlist-ready",
-          "true"
-        );
+    if (!groupClass) {
+      return;
+    }
 
-        var pseudo = memberCard.querySelector(
-          "[data-litso-memberlist_pseudo]"
-        );
+    pseudo.classList.add(groupClass);
 
-        if (!pseudo) {
-          return;
-        }
-
-        var groupMarker = pseudo.querySelector(
-          ".usr_grp_clr"
-        );
-
-        var groupClass = "";
-
-
-        /* ----- CLASSE PRÉSENTE DANS LE PSEUDO ----- */
-
-        if (groupMarker) {
-          groupClass = Array.prototype.find.call(
-            groupMarker.classList,
-            function (className) {
-              return className.indexOf("group-") === 0;
-            }
-          ) || "";
-        }
-
-
-        /* ----- SECOURS AVEC USER_GROUP_ID ----- */
-
-        if (!groupClass) {
-          var groupId = memberCard.getAttribute(
-            "data-group"
-          );
-
-          if (groupId) {
-            groupClass = "group-" + groupId;
-          }
-        }
-
-
-        /* ----- APPLICATION ----- */
-
-        if (groupClass) {
-          memberCard.classList.add(groupClass);
-          pseudo.classList.add(groupClass);
-        }
-      }
+    var memberCard = pseudo.closest(
+      ".litso-memberlist_member"
     );
-  }
 
+    if (memberCard) {
+      memberCard.classList.add(groupClass);
+    }
 
-  if (document.readyState === "loading") {
-    document.addEventListener(
-      "DOMContentLoaded",
-      initializeLitsoMemberlist,
-      { once: true }
-    );
-  } else {
-    initializeLitsoMemberlist();
-  }
-})();
+  });
+
+});
