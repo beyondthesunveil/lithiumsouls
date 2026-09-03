40c549604fd36806b85c98373a29b1c8507bee0b
veilsun. <87560864+beyondthesunveil@users.noreply.github.com>
2026-09-03 23:47:18 +0200

    Create charatab.js

---
+++ TEMPLATES/VIEWTOPIC BODY/charatab.js
@@ -0,0 +1,59 @@
+/*** -------------------- PROFIL SYSTEME ONGLET --------------- ***/
+
+.systab {
+  padding: 15px;
+}
+
+.tabs {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: -20px 0 10px;
+} 
+/* .systab .tab => les onglet */
+.systab .tab {
+  padding: 2px 11px;
+  margin-left: 1px;
+  color: #ccc;
+  background: var(--color03);
+  border: 1px solid var(--border);
+  border-radius: var(--mainradius);
+  font: 12px var(--font-text);
+  text-transform: uppercase;
+  font-weight: bold;
+  cursor: pointer;
+}
+/* .systab .tab:hover => les onglets survolés */
+.systab .tab:hover {
+  background: var(--gradientbis);
+  color: var(--colorfont);
+  border-color: none;
+  transition: 0.3s;
+}
+/* .systab .tab.selected => les onglets sélectionnés */
+.systab .tab.selected{
+  color: var(--colorfont);
+  font: 11px var(--font-text);
+  text-transform: uppercase;
+  height:18px;
+  font-weight: bold;
+  border: none;
+  transition: 0.3s;
+  background: var(--gradient);
+  border-radius: var(--mainradius);
+}
+/* .systab .contents => les conteneurs des contenus associés aux onglets  */
+.systab .contents {
+  color: var(--colorbis);
+  background: var(--color03);
+  border-radius: var(--mainradius);
+}
+/* style des onglets si ils sont à gauche ou à droite */
+.systab.s_float .tab,.systab.s_float .tab {
+  display: block;
+  margin: 5px;
+}
+/* flottement des conteneurs si les onglets sont à gauche où à droite */
+.systab.s_float .tabs,.systab.s_float .contents {
+  float: left;
+}
