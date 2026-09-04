ff9cfb53d0ee04b63a6d77b99d550da6486bd26c
veilsun. <87560864+beyondthesunveil@users.noreply.github.com>
2026-09-04 12:14:40 +0200

    Update profile.js

--- TEMPLATES/PROFILE VIEW BODY/profile.js
+++ TEMPLATES/PROFILE VIEW BODY/profile.js
@@ -1,5 +1,351 @@
-<script type="text/javascript">
-//<![CDATA[
+(function () {
+  "use strict";
+
+  function initializeLitsoProfile() {
+
+    var profile = document.querySelector(
+      "[data-litso-profile]"
+    );
+
+    if (
+      !profile ||
+      profile.getAttribute("data-litso-profile-ready") === "true"
+    ) {
+      return;
+    }
+
+    profile.setAttribute(
+      "data-litso-profile-ready",
+      "true"
+    );
+
+
+    /* =====================================================
+       ÉDITION AJAX DES CHAMPS
+       ===================================================== */
+
+    if (
+      !window.jQuery ||
+      typeof jQuery.toJSON !== "function"
+    ) {
+      return;
+    }
+
+    var $ = window.jQuery;
+
+
+    $(profile)
+      .find("[id^='field_id']")
+      .each(function () {
+
+        var field = $(this);
+
+        var editable = field.find(
+          ".field_editable"
+        );
+
+
+        if (!editable.is("span, div")) {
+          return;
+        }
+
+
+        /* -----------------------------------------------
+           APPARITION DU BOUTON D'ÉDITION
+           ----------------------------------------------- */
+
+        field.on(
+          "mouseenter",
+          function () {
+
+            if (
+              !field
+                .find(".field_editable.invisible")
+                .is("span, div")
+            ) {
+              return;
+            }
+
+
+            if (
+              field.find(".ajax-profil_edit").length
+            ) {
+              return;
+            }
+
+
+            field
+              .find(".field_uneditable")
+              .addClass("ajax-profil_hover");
+
+
+            field.addClass(
+              "ajax-profil_parent"
+            );
+
+
+            var editButton = $(
+              "<button>",
+              {
+                type: "button",
+                class: "ajax-profil_edit",
+                title: "{L_FIELD_EDIT_VALUE}",
+                "aria-label": "{L_FIELD_EDIT_VALUE}"
+              }
+            );
+
+
+            editButton.append(
+              $("<img>", {
+                src: "{AJAX_EDIT_IMG}",
+                alt: ""
+              })
+            );
+
+
+            field.append(editButton);
+
+
+            /* -------------------------------------------
+               PASSAGE EN MODE ÉDITION
+               ------------------------------------------- */
+
+            editButton.on(
+              "click",
+              function () {
+
+                field
+                  .find(".field_uneditable")
+                  .removeClass("ajax-profil_hover")
+                  .addClass("invisible");
+
+
+                field
+                  .find(".field_editable")
+                  .removeClass("invisible");
+
+
+                var validButton = $(
+                  "<button>",
+                  {
+                    type: "button",
+                    class: "ajax-profil_valid",
+                    title: "{L_VALIDATE}",
+                    "aria-label": "{L_VALIDATE}"
+                  }
+                );
+
+
+                validButton.append(
+                  $("<img>", {
+                    src: "{AJAX_VALID_IMG}",
+                    alt: ""
+                  })
+                );
+
+
+                field
+                  .find(".field_editable")
+                  .append(validButton);
+
+
+                editButton.remove();
+
+
+                /* ---------------------------------------
+                   VALIDATION
+                   --------------------------------------- */
+
+                validButton.on(
+                  "click",
+                  function () {
+
+                    var content = [];
+
+
+                    field
+                      .find(".field_editable [name]")
+                      .each(function () {
+
+                        var input = $(this);
+
+                        var special = input.is(
+                          "input[type='radio'], input[type='checkbox']"
+                        );
+
+
+                        if (
+                          !special ||
+                          input.is(":checked")
+                        ) {
+                          content.push([
+                            input.attr("name"),
+                            input.val()
+                          ]);
+                        }
+
+                      });
+
+
+                    var fieldId = field
+                      .attr("id")
+                      .substring(8);
+
+
+                    $.post(
+                      "{U_AJAX_PROFILE}",
+                      {
+                        id: fieldId,
+                        user: "{CUR_USER_ID}",
+                        active: "{CUR_USER_ACTIVE}",
+                        content: $.toJSON(content),
+                        tid: "{TID}"
+                      },
+                      function (data) {
+
+                        $.each(
+                          data,
+                          function (
+                            responseId,
+                            item
+                          ) {
+
+                            var target = $(
+                              "#field_id" + responseId
+                            );
+
+
+                            /* ---------------------------
+                               ERREUR
+                               --------------------------- */
+
+                            if (
+                              item.indexOf("error : ") === 0
+                            ) {
+
+                              var error = target.find(
+                                ".form-error"
+                              );
+
+
+                              if (!error.length) {
+
+                                error = $(
+                                  "<div>",
+                                  {
+                                    class: "form-error"
+                                  }
+                                );
+
+
+                                target
+                                  .find(".field_editable")
+                                  .append(error);
+
+                              }
+
+
+                              error.text(
+                                item.substring(8)
+                              );
+
+                              return;
+                            }
+
+
+                            /* ---------------------------
+                               SUCCÈS
+                               --------------------------- */
+
+                            target
+                              .find(".form-error")
+                              .remove();
+
+
+                            target
+                              .find(".field_uneditable")
+                              .html(item)
+                              .removeClass("invisible");
+
+
+                            target
+                              .find(".field_editable")
+                              .addClass("invisible");
+
+
+                            target
+                              .find(".ajax-profil_valid")
+                              .remove();
+
+                          }
+                        );
+
+                      },
+                      "json"
+                    );
+
+                  }
+                );
+
+              }
+            );
+
+          }
+        );
+
+
+        /* -----------------------------------------------
+           SORTIE DU CHAMP
+           ----------------------------------------------- */
+
+        field.on(
+          "mouseleave",
+          function () {
+
+            if (
+              field
+                .find(".field_editable")
+                .hasClass("invisible")
+            ) {
+
+              field
+                .find(".field_uneditable")
+                .removeClass("ajax-profil_hover");
+
+
+              field
+                .find(".ajax-profil_edit")
+                .remove();
+
+            }
+
+          }
+        );
+
+      });
+
+  }
+
+
+  if (
+    document.readyState === "loading"
+  ) {
+
+    document.addEventListener(
+      "DOMContentLoaded",
+      initializeLitsoProfile,
+      { once: true }
+    );
+
+  } else {
+
+    initializeLitsoProfile();
+
+  }
+
+})();
+
+/* <script type="text/javascript">
 (function () {
   "use strict";
 
@@ -433,3 +779,4 @@
 })();
 //]]>
 </script>
+*/
